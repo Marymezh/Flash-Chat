23b2c5159391286ab87fd70088c3eae5da459cea
Marymezh <maria.mezhova@yahoo.com>
2022-02-09 23:12:59 +0200

    FireBase SDK is added into the project and logics of registering of a new user is completed

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,39 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                
+                if let error = error {
+                    
+                    self.showAlert(error: error)
+                    
+                } else {
+                    
+                    self.performSegue(withIdentifier: "RegisterNewUser", sender: self)
+                    
+                }
+            }
+        }
+    }
+    
+    private func showAlert (error: Error) {
+        let alert = UIAlertController(title: "Warning", message: error.localizedDescription, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Retry", style: .cancel) { _ in
+            print("retry entry")
+        }
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
     }
     
 }

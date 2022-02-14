60f0dd48ddce8ea4dc9a3ce7af236efe52cc665c
Marymezh <maria.mezhova@yahoo.com>
2022-02-14 17:10:31 +0200

    messages sending to and loading from Firestore DB logics in process

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,15 +14,61 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = K.appName
         navigationItem.hidesBackButton = true
         
+        tableView.dataSource = self
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
+        loadMessages()
+    }
+    
+    private func loadMessages() {
+        messages = []
+        
+        db.collection(K.FStore.collectionName).getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print ("There was an issue retrieving data from Firestore \(error.localizedDescription)")
+            } else {
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments {
+                        let data = doc.data()
+                        if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: messageSender, body: messageBody)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                                                                K.FStore.senderField : messageSender,
+                                                                K.FStore.bodyField: messageBody]) { (error) in
+                if let error = error {
+                    print("There was an issue saving data to firestore \(error.localizedDescription)")
+                } else {
+                    print ("Data is successfully saved")
+                }
+            }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -43,4 +89,17 @@ class ChatViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
 }
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,40 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Maria Mezhova on 10.02.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+        messageBubble.layer.shadowRadius = messageBubble.frame.size.height / 5
+        messageBubble.layer.shadowOffset = CGSize(width: 4, height: 4)
+        messageBubble.layer.shadowColor = UIColor.gray.cgColor
+        messageBubble.layer.shadowOpacity = 0.7
+        messageBubble.layer.masksToBounds = false
+        
+//        rightImageView.layer.shadowRadius = rightImageView.frame.size.height / 2 
+//        rightImageView.layer.shadowOffset = CGSize(width: 4, height: 4)
+//        rightImageView.layer.shadowColor = UIColor.gray.cgColor
+//        rightImageView.layer.shadowOpacity = 0.7
+//        rightImageView.layer.masksToBounds = false
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
